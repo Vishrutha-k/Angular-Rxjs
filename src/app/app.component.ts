14b8c40942a2d1dad070b248ce8dc9136ae09bfd
Vishrutha-k <vishruthakacham@gmail.com>
2021-05-26 19:00:33 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component, VERSION } from '@angular/core';
-
+import { Component, VERSION, OnInit } from '@angular/core';
+import { map, tap, take } from 'rxjs/operators';
+import { of, from, throwError } from 'rxjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  ngOnInit() {
+    of(1, 2, 3, 4, 0)
+      .pipe(
+        tap(x => `original item: ${x}`),
+        map(x => x * 2),
+        map(x => x - 0),
+        map(item => {
+          if (item === 0) {
+            throw new Error('Zero detected');
+          }
+          return item;
+        }),
+        take(7)
+      )
+      .subscribe(console.log);
+  }
 }
